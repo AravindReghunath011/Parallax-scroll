a42f68e37b920a357034f8f638db239f13235bc0
Aravind <aravindreghunath99@gmail.com>
2024-06-20 20:46:31 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Parallax from './Components/parallax'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <Parallax/>
+    </>
+  )
+}
+
+export default App
